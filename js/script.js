3fd0f35015a5627356dd06214f741c982eaf6978
Salvatore1985 <80257763+Salvatore1985@users.noreply.github.com>
2022-03-09 16:26:19 +0100

    aggiunto card dinamiche js

--- js/script.js
+++ js/script.js
@@ -51,28 +51,46 @@ const team = [
 /* MILESTONE 1:
 stampare su console le informazioni di nome, ruolo e la stringa della foto */
 //*STAMPO IN CONSOLE TUTTI GLI ELEMENTI DELL'ARRAY 
-console.log(team);
-
+/* console.log(team);
+ */
 //*STAMPO IN CONSOLE TUTTI I SINGOLI ELEMENTI DELL'ARRAY 
-console.log(team[0]);
+/* console.log(team[0]);
 console.log(team[1]);
 console.log(team[2]);
 console.log(team[3]);
 console.log(team[4]);
 console.log(team[5]);
+ */
 
+
+//*RECUPERO L'ELEMENTO DAL DOM
+const contentCardsElement = document.getElementById("content-cards");
 //*STAMPO IN CONSOLE CON UN "CICLO IN" TUTTI I SINGOLI ELEMENTI DELL'ARRAY 
 for (const chiave in team) {
-    console.log(team);
+    /*     console.log(team); */
+    contentCardsElement.innerHTML += ` <div class="my-card text-center  m-5">
+    <div>
+        <img class="img-fluid" src="img/${team[chiave].image}"alt="${team[chiave].name}">
+    </div>
+    <h3 class="py-3">${team[chiave].name}</h3>
+    <p>${team[chiave].role}</p>
+    </div>`;
 }
 
 
 
-/* createGridSquare = (myClass) */
 
+
+{/* <div class="my-card text-center  m-5">
+<div>
+    <img class="img-fluid" src="img/angela-caroll-chief-editor.jpg" alt="img-Angela">
+</div>
+<h3 class="py-3">nome</h3>
+<p>ruolo</p>
+</div> */}
 
 /**
- * Arrow function che restituisce un div di square sotto forma di elemento del DOM
+ * Arrow function che restituisce un div  sotto forma di elemento del DOM
  * @param {*} myClass la classe che vogliamo aggiungere al "div"
  * @returns 
  */
